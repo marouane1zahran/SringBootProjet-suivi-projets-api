c525564a2cc4919cd73e0353f2eb034dcb4f466f
marouane <zahranmarouane77@gmail.com>
2026-03-13 23:18:41 +0100

    Initialisation du projet Spring Boot et de l'architecture métier

---
+++ src/test/java/ma/spring/suiviprojet/SuiviprojetApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.spring.suiviprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiviprojetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
