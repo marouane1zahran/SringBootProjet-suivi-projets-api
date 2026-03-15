efa3b4f81bfa651d957ce792bd013ab41bc46e98
abderrahmane souaiki <abdousouaiki000@gmail.com>
2026-03-15 02:56:38 +0200

    Add employe API with automapping and profil relation

---
+++ src/main/java/ma/spring/suiviprojet/organisation/dto/EmployeResponseDTO.java
@@ -0,0 +1,15 @@
+package ma.spring.suiviprojet.organisation.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeResponseDTO {
+
+    private Long id;
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+
+}
